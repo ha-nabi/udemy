892d10149ae5625515d49dd4ac16b78b246ed9eb
ciu <112779139+kangciu@users.noreply.github.com>
2023-12-05 23:11:31 +0900

    Initial Commit

---
+++ Asyncimage/AsyncimageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncimageApp.swift
+//  Asyncimage
+//
+//  Created by 강치우 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncimageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Asyncimage/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Asyncimage
+//
+//  Created by 강치우 on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    var body: some View {
+        // MARK: - 1. BASIC
+        
+//        AsyncImage(url: URL(string: imageURL))
+        
+        // MARK: - 2. SCALE
+        
+//        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        // MARK: - 3. PLACEHOLDER
+        
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 128)
+                .foregroundStyle(.purple)
+                .opacity(0.5)
+        }
+        .padding(40)
+    }
+}
+
+#Preview {
+    ContentView()
+}
